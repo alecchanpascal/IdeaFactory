ccbd6fd3b404b3f536d465249a3af6d344086dd8
Alec Chan <alec.pascal@gmail.com>
2022-08-03 19:38:57 +0000

    added seeding 'cause why not it's already late

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Review.destroy_all
+Idea.destroy_all
+User.destroy_all
+
+10.times do 
+    first_name = Faker::Name.first_name
+    last_name = Faker::Name.last_name
+    User.create(
+        name: first_name + " " + last_name,
+        email: "#{first_name}@#{last_name}.com",
+        password: "123"
+    )
+end
+  
+users = User.all
+
+50.times do
+    i = Idea.create(
+        title: Faker::FunnyName.name,
+        description: Faker::Lorem.paragraph,
+        user: users.sample
+    )
+    if i.valid?
+        rand(1..5).times do
+            Review.create(body: Faker::Hacker.say_something_smart, idea:i, user: users.sample)
+        end
+    end
+end
